7f013b0eaf564dd6d4c53de5d182b636db899673
Bilal-S <bilal.soylu[at]gmail.com>
2020-06-23 14:04:56 -0400

    upload project 1.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# example-payment-sdk-ionic
-Ionic Framework with XcooBee
+# Ionic Framework with XcooBee Contactless Payment
+
+This example uses the [XcooBee Payments SDK](https://github.com/XcooBee/payment-sdk-js) for JavaScript (Node JS & Browser).
+Please clone to a directory on your system or copy example files from the SDK into separate directory.
+
+This example project was build with Ionic [Ionic](https://ionicframework.com/) CLI. 
+The SDK is plain ES6 JS and has no dependencies on the Ionic Framework so you can use it any browser based application or node js application. 
+
+The SDK has support for both.
+
+You will need to install the Ionic command line tools to run this example.
+
+
+```
+npm install -g @ionic/cli
+```
+
+## SDK
+
+Information about SDK and its API is available in SDK Repo.
+
+Please see [SDK](https://github.com/XcooBee/payment-sdk-js)
+
+
+## Install
+
+Use the `npm install` command in your terminal window after you have navigated to the project directory.
+
+## Run
+
+```
+ionic serve
+```
+
+
+If your browser does not automatically open after you see `Files successfully emitted..` message in console try to manually go to the start page: 
+`http://localhost:8100/home`
---
+++ src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonFooter, IonRow, IonCol, IonItem, IonInput, IonImg } from '@ionic/react';
+import React, {useEffect, useState } from 'react';
+import { BrowserXcooBeePaymentSDK } from '@xcoobee/payment-sdk';
+import './Home.css';
+
+// init XcooBee
+const sdk = new BrowserXcooBeePaymentSDK({ campaignId: "test", formId: "test" });
+
+const Home: React.FC = () => {
+
+  const [chargeAmount, setNumber] = useState<number>(123.00);  
+  const [qr, setQr] = useState<string>();
+
+  useEffect(() => {
+    sdk.createPayQr({ amount: chargeAmount, reference: "Order" }, 300).then(base64 => setQr(base64));
+    }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Contactless Payment</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="myphoto">
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Contactless Payment</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonRow >
+          <IonCol className="ion-text-center">
+            How much would you like to charge?
+            <IonItem>
+              <IonInput type="number" value={chargeAmount} placeholder="Enter Amount" onIonChange={e => setNumber(parseInt(e.detail.value!, 10))}></IonInput>
+            </IonItem>            
+            <IonButton color="primary" onClick={e => {sdk.createPayQr({ amount: chargeAmount, reference: "Order" }, 300).then(base64 => setQr(base64));
+    }}>Generate</IonButton>
+          </IonCol>
+        </IonRow>
+        
+        {/* qr image space */}
+        <div className="vertical-center"> 
+          <div className="center"> 
+            <IonImg src={qr} className="ion-align-self-end" />        
+          </div> 
+        </div>
+
+      </IonContent>
+      <IonFooter>
+        <b>https://www.xcoobee.com</b>
+      </IonFooter>
+    </IonPage>
+  );
+};
+
+export default Home;
